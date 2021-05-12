c1afb9cddf26cc7cc1624544c8ad5fbb0989b149
Harshil Bhatt <harshilbhatt2001@gmail.com>
2021-05-13 01:10:29 +0530

    ekf on odom and imu

--- tribot_gazebo/launch/tribot_gazebo.launch.py
+++ tribot_gazebo/launch/tribot_gazebo.launch.py
@@ -5,16 +5,14 @@ from launch import LaunchDescription
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-import xacro
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='tribot_description').find('tribot_description')
+    #pkg_share = FindPackageShare(package='tribot_description').find('tribot_description')
+    pkg_share = get_package_share_directory('tribot_description')
     default_model_path = os.path.join(pkg_share, 'urdf', 'tribot.urdf')
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    xacro_path = os.path.join(get_package_share_directory("tribot_description"), 'urdf', 'tribot.urdf.xacro')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -29,38 +27,32 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('gui'))
     )
 
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[os.path.join(pkg_share, 'config', 'ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
-
-
-    # Gazebo Launch
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
-        )
-    )
-
+    
     spawn_entity = Node(
-            package='tribot_gazebo',
-            executable='inject_entity.py',
-            output='screen',
-        )
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'tribot', '-topic', 'robot_description'],
+        output='screen'
+    )
+
 
     return LaunchDescription([
-        DeclareLaunchArgument(name='use_sim_time', default_value='false',
+        DeclareLaunchArgument(name='use_sim_time', default_value='True',
                               description='Use simulation (Gazebo) clock if true'),
         DeclareLaunchArgument(name='model', default_value=default_model_path,
                               description='Absolute path to robot urdf file'),
         DeclareLaunchArgument(name='gui', default_value='false',
                               description='Flag to enable joint_state_publisher_gui'),
-        # TODO: Gazebo doesn't load
-        # gazebo, 
+        ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'], output='screen'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        #spawn_entity,
+        spawn_entity,
+        robot_localization_node,
     ])
