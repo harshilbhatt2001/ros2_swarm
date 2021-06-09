3f7bbf00198715de033817185658a09e777abb12
Harshil Bhatt <harshilbhatt2001@gmail.com>
2021-06-09 17:43:19 +0530

    added robot_description parameter in fake node robot state publisher

--- tribot_simulations/tribot_fake_node/launch/tribot_fake_node.launch.py
+++ tribot_simulations/tribot_fake_node/launch/tribot_fake_node.launch.py
@@ -1,8 +1,10 @@
 import os
 
+import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 
@@ -16,8 +18,12 @@ def generate_launch_description():
     urdf_file_name = 'tribot.urdf'
     urdf = os.path.join(
         get_package_share_directory('tribot_description'), 'urdf', urdf_file_name)
+    doc = xacro.parse(open(urdf))
+    xacro.process_doc(doc)
+    urdf = doc.toxml()
     
     return LaunchDescription([
+
         Node(
             package='tribot_fake_node',
             executable='tribot_fake_node',
@@ -29,8 +35,8 @@ def generate_launch_description():
             name='robot_state_publisher',
             output='screen',
             parameters=[
-                {'use_sim_time': use_sim_time}
-            ],
-            arguments=[urdf]),
+                {'use_sim_time': use_sim_time},
+                {'robot_description': urdf}  
+            ]),
     ])
     
